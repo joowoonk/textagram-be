3137b20355154e70a7ad040788b6a07ab365bf89
Joo Woon Kang <jwk@Joos-MacBook-Pro.local>
2020-08-17 01:13:38 -0700

    created seeds for posts

--- database/seeds/user_admin.js
+++ database/seeds/user_admin.js
@@ -21,3 +21,8 @@ exports.seed = function (knex) {
 
   return knex("users").insert(users);
 };
+
+// knex seed:make seed_name
+// knex seed:make seed_name -x ts # Generates a TypeScript seed file
+// knex seed:run # Runs all seed files
+// knex seed:run --specific=seed-filename.js # Runs a specific seed file
