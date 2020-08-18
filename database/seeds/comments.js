87b0f9957c9d5c9c930a03306abd7ed336aeb3e9
Joo Woon Kang <jwk@Joos-MacBook-Pro.local>
2020-08-17 21:50:30 -0700

    comments seeds are now working and .http file of rest client extension tried and working fine

---
+++ database/seeds/comments.js
@@ -0,0 +1,34 @@
+// const secrets  = require("../../auth/secrets");
+exports.seed = function (knex) {
+  // 000-cleanup.js already cleaned out all tables
+
+  const comments = [
+    {
+      text: "first comments! Ooh hoo!",
+      post_id: 6,
+      user_id: 2,
+    },
+    {
+      text:
+        "Having three real friends in life is equivalent to having a successful life",
+      post_id: 6,
+      user_id: 3,
+    },
+    {
+      text: "Forest Gump is such a best movie in our life time",
+      post_id: 7,
+      user_id: 3,
+    },
+    {
+      text: "May the force be with you!",
+      post_id: 9,
+      user_id: 2,
+    },
+    {
+      text: "Nike probably got their slogan from Yoda 😉😉😉",
+      post_id: 9,
+      user_id: 1,
+    },
+  ];
+  return knex("comments").insert(comments);
+};
