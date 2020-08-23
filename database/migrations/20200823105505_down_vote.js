afaccc66c22be30a365c449699e4580923272252
Your Name <joowoonk@gmail.com>
2020-08-23 12:47:53 -0700

    added another table for down votes

--- database/migrations/20200823105505_down_vote.js
+++ database/migrations/20200823105505_down_vote.js
@@ -1,10 +1,10 @@
-exports.up = function (knex) {
-  return knex.schema.createTable("up_voted_post", (up_voted_post) => {
+exports.up = async function (knex) {
+  await knex.schema.createTable("down_voted_post", (down_voted_post) => {
     //Making a primary key. This will combine id of both tables and come up with a unique id
-    up_voted_post.primary(["user_id", "post_id"]);
-    up_voted_post.timestamp("up_voted_at").defaultTo(knex.fn.now());
-    up_voted_post.boolean("voted").defaultTo(0);
-    up_voted_post
+    down_voted_post.primary(["user_id", "post_id"]);
+    down_voted_post.timestamp("down_voted_at").defaultTo(knex.fn.now());
+    down_voted_post.boolean("down_voted").defaultTo(0);
+    down_voted_post
       .integer("post_id")
       .unsigned()
       .notNullable()
@@ -12,7 +12,7 @@ exports.up = function (knex) {
       .inTable("posts")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
-    up_voted_post
+    down_voted_post
       .integer("user_id")
       .unsigned()
       .notNullable()
@@ -23,6 +23,6 @@ exports.up = function (knex) {
   });
 };
 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists("up_voted_post");
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("up_voted_post");
 };
